6f7acf54228b946f52fc0f052c16e1bd3b5cd893
Robert Fidler <robertf224@gmail.com>
2015-05-10 20:33:39 -0400

    added genomes

--- compute.py
+++ compute.py
@@ -1,2 +1,29 @@
-import sys
+import os, cPickle, random, heapq, sys
+from utils import kmer_store, kmers, nucleotides_fna, progress
+import numpy as np
+import scipy.optimize as op
 
+"""
+	usage: compute <k> <sample filename>
+"""
+k = int(sys.argv[1])
+sample_filename = sys.argv[2]
+
+kmer_spectra_filename = 'pickles/kmer_spectra_%d.pickle' % k
+with open(kmer_spectra_filename) as f:
+	kmer_spectra = cPickle.load(f)
+
+# get totals for each kmer
+kmer_totals = [0]*20
+for kmer in kmer_spectra:
+	for index, count in enumerate(kmer_spectra[kmer]):
+		kmer_totals[index] += count
+
+reads = open(sample_filename)
+read_map_counts = [0]*20
+for read in reads:
+	# choose best match in genomes
+	for kmer in kmers(read):
+
+
+estimated_proportions
